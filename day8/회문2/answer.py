9f752c0edd852990c4bc1db826777b295a2f978b
HJH13579 <heojoohyeok@naver.com>
2023-02-10 17:53:46 +0900

    add change

---
+++ day8/회문2/answer.py
@@ -0,0 +1,8 @@
+# 최대값의 경우의 수
+#
+# 길이 : 100 ~ 2 감소시키면 찾기
+#
+# for M (N, 1, -1)
+#     if isPal(arr, M) or isPal(arr_t, M)
+#         break
+
--- day8/회문2/palindrome2.py
+++ day8/회문2/palindrome2.py
@@ -4,25 +4,16 @@ sys.stdin = open("input.txt", "r", encoding="UTF8")
 
 def palindrome(ar1, ar2):
     ans = 1
-
     for x in range(100, 0, -1):
         for i in range(100):
             for j in range(100 - x + 1):
                 # RunTime Error
-                # if ar1[i][j:j + x] == ar1[i][j:j + x][::-1] or ar2[i][j:j + x] == ar2[i][j:j + x][::-1]:
-
-                for y in range(x // 2):
-                    if ar1[i][j + y] == ar1[i][x - (j+y)] or ar2[i][j + y] == ar2[i][x - (j+y)]:
-                        ans = x
-                        return ans
+                if ar1[i][j:j + x] == ar1[i][j:j + x][::-1] or ar2[i][j:j + x] == ar2[i][j:j + x][::-1]:
+                    ans = x
+                    return ans
 
 for tck in range(1, 11):
-
-    _ = int(input())
-
-    arr = [list(input()) for _ in range(100)]
-    arr_t = list(map(list, zip(*arr)))
-
-    result = palindrome(arr, arr_t)
-
-    print(f'#{tck} {result}')
+    _ = input()
+    arr = [input() for _ in range(100)]
+    arr_t = list(zip(*arr))
+    print(f'#{tck} {palindrome(arr, arr_t)}')
